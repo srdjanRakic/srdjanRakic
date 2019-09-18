8323c73cd85165bbae04e9ca3f711f151f720d5b
srdjanRakic <srgjan.rakic@gmail.com>
2019-09-18 14:20:43 +0200

    Rewrite TemplateWrapper to func. component

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,45 +1,34 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
-import get from 'lodash/get'
 import './style.scss'
+
+// images
 import profileImage from '../assets/images/srdjan-rakic.jpg'
 import favicon16 from '../assets/favicons/favicon-16x16.png'
 import favicon32 from '../assets/favicons/favicon-32x32.png'
 
-class TemplateWrapper extends React.Component {
-  render () {
-    const siteTitle = get(this, 'props.data.site.siteMetadata.title')
-    const siteKeywords = get(this, 'props.data.site.siteMetadata.keywords')
-    const siteURL = get(this, 'props.data.site.siteMetadata.url')
-    const siteDescription = get(this, 'props.data.site.siteMetadata.description')
-
-    const { children } = this.props
-
-    return (
-      <div className="template-wrapper">
-        <Helmet
-          title={siteTitle}
-          meta={[
-            { name: 'description', content: siteDescription },
-            { name: 'keywords', content: siteKeywords },
-            { property: 'og:url', content: siteURL },
-            { property: 'og:image', content: profileImage },
-            { property: 'og:title', content: siteTitle },
-            { property: 'og:description', content: siteDescription },
-          ]}
-          link={[
-            { rel: 'icon', type: 'image/png', sizes: '16x16', href: favicon16 },
-            { rel: 'icon', type: 'image/png', sizes: '32x32', href: favicon32 }
-          ]}
-        />
-        <div className="template-wrapper-children">
-          { children() }
-        </div>
-      </div>
-    )
-  }
-}
+const TemplateWrapper = ({ children, data: { site: { siteMetadata: { title, keywords, url, description } } } }) => (
+  <div className="template-wrapper">
+    <Helmet
+      title={title}
+      meta={[
+        { name: 'description', content: description },
+        { name: 'keywords', content: keywords },
+        { property: 'og:url', content: url },
+        { property: 'og:image', content: profileImage },
+        { property: 'og:title', content: title },
+        { property: 'og:description', content: description },
+      ]}
+      link={[
+        { rel: 'icon', type: 'image/png', sizes: '16x16', href: favicon16 },
+        { rel: 'icon', type: 'image/png', sizes: '32x32', href: favicon32 }
+      ]}
+    />
+    <div className="template-wrapper-children">
+      { children() }
+    </div>
+  </div>
+);
 
 export default TemplateWrapper
 
