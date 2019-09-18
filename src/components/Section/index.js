9eba4e514e8a266b55c432b6123c194db74bb8b4
srdjanRakic <srgjan.rakic@gmail.com>
2019-09-18 14:05:01 +0200

    Rewrite Section as func comp.

--- src/components/Section/index.js
+++ src/components/Section/index.js
@@ -1,19 +1,15 @@
 import React, {Component} from 'react'
 import './style.scss';
 
-class Section extends Component {
-  render () {
-    return (
-      <div className="section">
-        <h6 className="bold">
-          {this.props.title}
-        </h6>
-        <div className="section-units">
-          {this.props.children}
-        </div>
-      </div>
-    )
-  }
-}
+const Section = () => (
+  <div className="section">
+    <h6 className="bold">
+      {this.props.title}
+    </h6>
+    <div className="section-units">
+      {this.props.children}
+    </div>
+  </div>
+)
 
 export default Section
