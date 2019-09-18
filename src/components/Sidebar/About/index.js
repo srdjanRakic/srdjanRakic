d19492c9fa3ef0b33df5a1ca5cba90edf7b5dfb7
srdjanRakic <srgjan.rakic@gmail.com>
2019-09-18 14:22:56 +0200

    Rewrite Links as func comp.

--- src/components/Sidebar/About/index.js
+++ src/components/Sidebar/About/index.js
@@ -2,12 +2,12 @@ import React, { Component } from 'react'
 import './style.scss'
 
 const About = () => (
-     <div className="about">
-        <div className="image" />
-        <div className="bio">
-          Software developer with a passion for making incredible web applications.
-        </div>
-      </div>
+  <div className="about">
+    <div className="image" />
+    <div className="bio">
+      Software developer with a passion for making incredible web applications.
+    </div>
+  </div>
 );
 
 export default About;
