e3b7bd535618fb1dbebdd10358d88b8ac55626c1
srdjanRakic <srgjan.rakic@gmail.com>
2019-09-18 14:03:29 +0200

    Change About to func. component

--- src/components/Sidebar/About/index.js
+++ src/components/Sidebar/About/index.js
@@ -1,17 +1,13 @@
 import React, { Component } from 'react'
-
 import './style.scss'
 
-class About extends Component {
-  render () {
-    return <div className="about">
+const About = () => (
+     <div className="about">
         <div className="image" />
         <div className="bio">
           Software developer with a passion for making incredible web applications.
-          <div className="emoji">🤖 &nbsp;🏔&nbsp;🎨&nbsp; ⚽️</div>
         </div>
-      </div>;
-  }
-}
+      </div>
+);
 
-export default About
+export default About;
