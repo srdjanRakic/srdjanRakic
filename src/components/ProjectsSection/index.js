0f7d8599e44316a19059d111a75b11e88a14b55f
srdjanRakic <srgjan.rakic@gmail.com>
2019-11-19 09:28:54 +0100

    Add hook cook book project

--- src/components/ProjectsSection/index.js
+++ src/components/ProjectsSection/index.js
@@ -7,6 +7,7 @@ import ExperienceUnit from '../ExperienceUnit'
 // logo
 import inplayerLogo from '../../assets/images/experience/inplayer-logo.png'
 import reactTsLogo from '../../assets/images/react-typescript.png'
+import reactHooksLogo from '../../assets/images/react-hooks.png'
 
 const ProjectsSection = () =>  (
       <Section title="Creator/Maintainer of:">
@@ -31,6 +32,13 @@ const ProjectsSection = () =>  (
             title="React TypeScript Starter"
             link="https://github.com/srdjanRakic/react-typescript-starter"
             subtitle="React TypeScript Starter Kit."
+          />
+          <ExperienceUnit
+            logo={reactHooksLogo}
+            colour="#FFFFFF"
+            title="The Hook Cook Book"
+            link="https://github.com/srdjanRakic/hook-cook-book"
+            subtitle="A collection of simple solution to complex problems using React Hooks"
           />
         </div>
       </Section>
