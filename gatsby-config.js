8946f79b9d59e372f2b7d5823b59f3f4fdc14d43
Srdjan Rakic <srgjan.rakic@gmail.com>
2022-04-07 23:06:51 +0200

    update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,19 +1,14 @@
 module.exports = {
   plugins: [
     "gatsby-plugin-sass",
+    "gatsby-plugin-sharp",
+    "gatsby-plugin-react-helmet",
     {
       resolve: `gatsby-plugin-google-fonts`,
       options: {
         fonts: [`open sans\:300,600`]
       }
     },
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        trackingId: "<your-tracking-id-here>",
-        head: true
-      }
-    }
   ],
   siteMetadata: {
     title: "Srdjan Rakic",
