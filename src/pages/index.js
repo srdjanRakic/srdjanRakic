e5ba368e84045ec58cc62cf63e23e28a435e04ac
Srdjan Rakic <srgjan.rakic@gmail.com>
2022-04-11 22:23:59 +0200

    remove redundant div

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,27 +8,27 @@ import Links from '../components/Sidebar/Links'
 import './style.scss'
 
 const IndexPage = () => (
-    <div className="index">
-      <div className="main">
-        <h5>
-          Hi, I'm <span className="bold">Srdjan Rakic</span>
-        </h5>
-        <h4 className="bold">
-          Software Engineer at Lightspeed Commerce
-        </h4>
-        <ExperienceSection />
-        <ProjectsSection />
-        <ConferenceSection />
+  <>
+    <div className="main">
+      <h5>
+        Hi, I'm <span className="bold">Srdjan Rakic</span>
+      </h5>
+      <h4 className="bold">
+        Software Engineer at Lightspeed Commerce
+      </h4>
+      <ExperienceSection />
+      <ProjectsSection />
+      <ConferenceSection />
+    </div>
+    <div className="aside">
+      <div className="top">
+        <About />
       </div>
-      <div className="aside">
-        <div className="top">
-          <About />
-        </div>
-        <div className="bottom">
-          <Links />
-        </div>
+      <div className="bottom">
+        <Links />
       </div>
     </div>
+  </>
 )
 
 export default IndexPage
