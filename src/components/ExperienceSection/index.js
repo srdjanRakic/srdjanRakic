eb4933c7316c168120bc72efa133daa0dbe84a76
Srgjan <srgjan.rakic@gmail.com>
2022-11-08 14:19:48 +0100

    fix: month names

--- src/components/ExperienceSection/index.js
+++ src/components/ExperienceSection/index.js
@@ -15,13 +15,13 @@ import silverflowLogo from "../../assets/images/experience/silverflow-logo.svg";
 const ExperienceSection = () => (
   <Section title="Experience:">
     <div className="row">
-      <Shuffle logo={mcaLogo} colour="#FFFFFF" title="MCA" link="https://www.mca.mk/" timeperiod="Feb 2016 - Jun 2017" description="Learnt the ins and outs of product development and
+      <Shuffle logo={mcaLogo} colour="#FFFFFF" title="MCA" link="https://www.mca.mk/" timeperiod="Feb 2016 - June 2017" description="Learnt the ins and outs of product development and
           built various features including a completely new product" />
-      <Shuffle logo={seavusLogo} colour="#FFFFFF" title="Seavus" link="https://seavus.com/" timeperiod="Jun 2017 - Dec 2018" description="Worked as a Product Engineer on the front-end on a Sports-Betting platform that provides a highly customizable front-end for each bookmaker, giving you all the tools you need for risk management" />
+      <Shuffle logo={seavusLogo} colour="#FFFFFF" title="Seavus" link="https://seavus.com/" timeperiod="June 2017 - Dec 2018" description="Worked as a Product Engineer on the front-end on a Sports-Betting platform that provides a highly customizable front-end for each bookmaker, giving you all the tools you need for risk management" />
       <Shuffle logo={inscaleLogo} colour="#FFFFFF" title="InScale" link="https://www.inscale.net/" timeperiod="Dec 2017 - Aug 2018" description="Worked as a Product Engineer on a scandinavian package holidaymaker" />
       <Shuffle logo={inplayerLogo} colour="#FFFFFF" title="InPlayer" link="https://inplayer.com/" timeperiod="Aug 2018 - Feb 2021" description="Worked as e Front-end Team Lead on a next generation video and digital asset monetization platform" />
-      <Shuffle logo={lightspeedLogo} colour="#FFFFFF" title="Lightspeed Commerce" link="https://www.lightspeedhq.nl/" timeperiod="Feb 2021 - Jun 2022" description="Working as e Product Engineer on a complete entrepreneurial platform for hospitality, retail and e-Commerce worldwide" />
-      <Shuffle logo={silverflowLogo} colour="#FFFFFF" title="Silverflow" link="https://www.silverflow.co/" timeperiod="Jun 2022 - present" description="Working as e Product Engineer on a innovative global payment technology for all payment service providers" />
+      <Shuffle logo={lightspeedLogo} colour="#FFFFFF" title="Lightspeed Commerce" link="https://www.lightspeedhq.nl/" timeperiod="Feb 2021 - July 2022" description="Working as e Product Engineer on a complete entrepreneurial platform for hospitality, retail and e-Commerce worldwide" />
+      <Shuffle logo={silverflowLogo} colour="#FFFFFF" title="Silverflow" link="https://www.silverflow.co/" timeperiod="July 2022 - present" description="Working as e Product Engineer on a innovative global payment technology for all payment service providers" />
     </div>
   </Section>
 );
